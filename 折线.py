b3b2862dccaa869b18f1674de285e2894e27ba03
BIRD <sunnywangpeng@sina.cn>
2017-02-09 15:29:56 +0800

    push code
    左下角为(0,0)

--- 折线.py
+++ 折线.py
@@ -2,7 +2,7 @@ from reportlab.graphics import renderPDF
 from reportlab.graphics.shapes import Drawing,PolyLine
 d = Drawing(300,300)
 s1 = PolyLine([(100,100),(100,200),(200,200),(200,100),(100,100)])
-s2 = PolyLine([(233,233),(267,267),(267,233),(233,267)])
+s2 = PolyLine([(133,133),(167,167),(167,133),(133,167)])
 d.add(s1)
 d.add(s2)
 renderPDF.drawToFile(d,'zhexian.pdf','A simple PDF file')
